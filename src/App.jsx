ed7a09dbcd38c9ed121c550b253c91bd20310e4f
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-07-14 21:10:24 -0400

    created App component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './Components/Header/Header'
+import Navbar from './Components/Navbar/Navbar'
+import Profile from './Components/Profile/Profile'
+import Dialogs from './Components/Dialogs/Dialogs'
+
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      {/* <Profile /> */}
+      <div className='app-wrapper-content'>
+        <Dialogs />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;

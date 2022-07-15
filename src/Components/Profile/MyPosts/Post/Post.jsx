89d147998f44e8fb0bd873d9d99d703211f6b28a
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-07-14 21:34:35 -0400

    My Post component (single post) with basic styling

---
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,12 @@
+import style from './Post.module.css';
+
+const Post = () => {
+    return(
+        <div className={style.item}>
+          <img src="https://banner2.cleanpng.com/20180623/iqh/kisspng-computer-icons-avatar-social-media-blog-font-aweso-avatar-icon-5b2e99c40ce333.6524068515297806760528.jpg"></img>
+          Post 1 bla bla bla
+        </div>
+    )
+}
+
+export default Post;

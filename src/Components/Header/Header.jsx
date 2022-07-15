b843d64b655c700bccf61b6e0ac7468c600a7a3d
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-07-14 21:36:23 -0400

    Header component with basic styling

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import s from './Header.module.css'
+
+const Header = () => {
+    return(
+        <div className={s.header}>
+          <img src='https://static.vecteezy.com/system/resources/previews/001/191/987/non_2x/circle-logo-png.png' alt='logo'></img>
+        </div>
+        
+    );
+}
+
+export default Header
